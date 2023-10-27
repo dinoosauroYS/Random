988d90624e810fd2efd1ede2dc0fad53d26596ab
dinoosauroYS <felipehfigueiredo@gmail.com>
2023-10-26 23:37:15 -0300

    fiz o programa criar 2 numeros aleatorios

--- src/App.java
+++ src/App.java
@@ -1,9 +1,15 @@
 import java.util.Random;
 import java.util.Scanner;
 public class App {
-    public static void main(String[] args) throws Exception {
-    sc close;
-        Random gerador = new Random(); 
+    public static void main(String[] args) { 
+        
+        Random gerador = new Random();
+
+        for (int i = 0; i < 2; i++) {
+            System.out.println(gerador.nextInt(26));
+            
         Scanner sc = new Scanner(System.in);
+        sc close;
+        }
     }
 }

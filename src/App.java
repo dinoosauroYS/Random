a7be457efa2fe64f21e2c366c91c371805ad74ec
dinoosauroYS <felipehfigueiredo@gmail.com>
2023-10-29 16:13:23 -0300

    alterei e arrumei os codigos

--- src/App.java
+++ src/App.java
@@ -22,6 +22,7 @@ public class App {
        }
        else
         System.out.println("voce errou:(");
-        sc close;
+        
         }
+        
     }
--- src/sc.java
+++
@@ -1,8 +0,0 @@
-
-public class sc {
-
-    public static int nextInt() {
-        return 0;
-    }
-
-}

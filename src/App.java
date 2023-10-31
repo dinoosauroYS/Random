3131456010cb5cc3ef06b1f2079df146a82a1a33
dinoosauroYS <felipehfigueiredo@gmail.com>
2023-10-30 23:55:42 -0300

    adicionei a dificuldade apenas fazer funcionar agora

--- src/App.java
+++ src/App.java
@@ -6,6 +6,21 @@ public class App {
         Random gerador = new Random();
         Scanner sc = new Scanner(System.in);
 
+        System.out.println("Selecione uma dificuldade: " + "easy, " + "medium, " + "hard");
+        String dificuldade = sc.next();
+
+        if (dificuldade == "easy") {
+            System.out.println("dificuldade escolhida: easy");
+            int nivel = 25;
+        }
+        
+        if (dificuldade == "medium") {
+            int nivel = 50;
+        }
+        if (dificuldade == "hard") {
+            int nivel = 100;
+            
+        }
        int valor_1 = gerador.nextInt(25);
        int valor_2 = gerador.nextInt(25);
        
@@ -15,6 +30,7 @@ public class App {
        int x = sc.nextInt();
 
        System.out.println("sua resposta é:" + x);
+       sc.close();
        
        if (x == valor_1 + valor_2) {
         

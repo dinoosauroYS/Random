fd4dc07705d5921ec5ca41f8012030e2df938728
dinoosauroYS <felipehfigueiredo@gmail.com>
2023-11-02 23:50:09 -0300

    deixei em looping o calculo

--- src/App.java
+++ src/App.java
@@ -1,79 +1,61 @@
 import java.util.Random;
 import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) { 
-        
+    public static void main(String[] args) {
+
         Random gerador = new Random();
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Selecione uma dificuldade: " + "1, " + "2, " + "3");
         int dificuldade = sc.nextInt();
+        int valor_1 = 0;
+        int valor_2 = 0;
+do{
+            if (dificuldade == 1) {
 
-        if (dificuldade == 1) {
             System.out.println("dificuldade escolhida: easy");
 
-            int valor_1 = gerador.nextInt(25);
-            int valor_2 = gerador.nextInt(25);
-             System.out.println("Calcule:");
-       System.out.println(valor_1 + " + " + valor_2);
-       
-       int x = sc.nextInt();
+            valor_1 = gerador.nextInt(25);
+            valor_2 = gerador.nextInt(25);
 
-       System.out.println("sua resposta é:" + x);
-       sc.close();
-       
-       if (x == valor_1 + valor_2) {
-        
-        System.out.println("voce acertou!");
-       }
-       else
-        System.out.println("voce errou:(");
         }
-        
-        if (dificuldade == 2) {
+
+        else if (dificuldade == 2) {
+
             System.out.println("dificuldade escolhida: medium");
 
-            int valor_1 = gerador.nextInt(50);
-            int valor_2 = gerador.nextInt(50);
-             System.out.println("Calcule:");
-       System.out.println(valor_1 + " + " + valor_2);
-       
-       int x = sc.nextInt();
+            valor_1 = gerador.nextInt(50);
+            valor_2 = gerador.nextInt(50);
 
-       System.out.println("sua resposta é:" + x);
-       sc.close();
-       
-       if (x == valor_1 + valor_2) {
-        
-        System.out.println("voce acertou!");
-       }
-       else
-        System.out.println("voce errou:(");
         }
-        if (dificuldade == 3) {
+         else if (dificuldade == 3) {
+
             System.out.println("dificuldade escolhida: hard");
 
-            int valor_1 = gerador.nextInt(100);
-            int valor_2 = gerador.nextInt(100);
-             System.out.println("Calcule:");
-       System.out.println(valor_1 + " + " + valor_2);
-       
-       int x = sc.nextInt();
+            valor_1 = gerador.nextInt(100);
+            valor_2 = gerador.nextInt(100);
 
-       System.out.println("sua resposta é:" + x);
-       sc.close();
-       
-       if (x == valor_1 + valor_2) {
-        
-        System.out.println("voce acertou!");
-       }
-       else
-        System.out.println("voce errou:(");
         }
-       
-       
-      
-        
+        else  {
+            System.out.println("utilize uma dificuldade elegivel");
+
         }
-        
+
+        System.out.println("Calcule:");
+        System.out.println(valor_1 + " + " + valor_2);
+
+        int x = sc.nextInt();
+
+        System.out.println("sua resposta é:" + x);
+
+        if (x == valor_1 + valor_2) {
+
+            System.out.println("voce acertou!");
+        } else
+            System.out.println("voce errou:(");
+    }while (true);
+    
+    
     }
+}
